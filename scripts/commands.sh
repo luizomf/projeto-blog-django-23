49ac58f943934fe73bafbacd0054928613e74b52
Luiz Otávio <luizomf@gmail.com>
2023-05-14 11:45:21 -0300

    Configurando os containers do Docker com Docker Compose (docker-compose.yml)

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,12 +4,13 @@
 set -e
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." &
-  sleep 0.1
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
 done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-python manage.py collectstatic
-python manage.py migrate
-python manage.py runserver
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
