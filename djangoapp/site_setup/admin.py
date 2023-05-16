354d131fd35b3973f082931a89683f4dbcaf23de
Luiz Otávio <luizomf@gmail.com>
2023-05-16 05:19:43 -0300

    Criando o model SiteSetup e registrando na admin do Django

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 
 @admin.register(MenuLink)
@@ -7,3 +7,11 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path',
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -12,3 +12,22 @@ class MenuLink(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class SiteSetup(models.Model):
+    class Meta:
+        verbose_name = 'Setup'
+        verbose_name_plural = 'Setup'
+
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=255)
+
+    show_header = models.BooleanField(default=True)
+    show_search = models.BooleanField(default=True)
+    show_menu = models.BooleanField(default=True)
+    show_description = models.BooleanField(default=True)
+    show_pagination = models.BooleanField(default=True)
+    show_footer = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
