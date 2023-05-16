531f2111580495d17ec872b7c76f0b24cc9a6df9
Luiz Otávio <luizomf@gmail.com>
2023-05-16 05:39:40 -0300

    Relação de ForeignKey e inlines com admin.TabularInline no Django

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path',
+#     list_display_links = 'id', 'text', 'url_or_path',
+#     search_fields = 'id', 'text', 'url_or_path',
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenuLinkInline,
 
     def has_add_permission(self, request):
         return not SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -9,6 +9,10 @@ class MenuLink(models.Model):
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        'SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
+        default=None,
+    )
 
     def __str__(self):
         return self.text
