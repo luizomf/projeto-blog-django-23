6641fed60b39ede58ac68edbf7a196b67a7083a5
Luiz Otávio <luizomf@gmail.com>
2023-05-16 07:15:49 -0300

    Criando um campo ImageField para Favicon no Django

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -33,5 +33,10 @@ class SiteSetup(models.Model):
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
 
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default=''
+    )
+
     def __str__(self):
         return self.title
