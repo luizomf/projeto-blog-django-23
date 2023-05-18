0101e41a060f39b40189a91f1e439b916f325896
Luiz Otávio <luizomf@gmail.com>
2023-05-18 07:22:27 -0300

    Layout do blog pronto e explicado em detalhes

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,22 +1,46 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
 
 
 def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/page.html'
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
 
 
 def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/post.html'
+        'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
