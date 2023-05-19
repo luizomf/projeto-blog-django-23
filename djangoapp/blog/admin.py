c09681cb57afcd8019100e200a7381fc41b83525
Luiz Otávio <luizomf@gmail.com>
2023-05-19 08:06:47 -0300

    Criando o model Page

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,4 +1,4 @@
-from blog.models import Category, Tag
+from blog.models import Category, Page, Tag
 from django.contrib import admin
 
 
@@ -23,4 +23,18 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = '-id',
     prepopulated_fields = {
         "slug": ('name',),
+    }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',),
     }
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -18,6 +18,9 @@ class Tag(models.Model):
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -34,3 +37,30 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
