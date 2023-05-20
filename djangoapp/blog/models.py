93a9b6833d3379a39c0442164247389041c4a583
Luiz Otávio <luizomf@gmail.com>
2023-05-20 09:15:47 -0300

    redimensionamento de imagens com o pillow

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from utils.images import resize_image
 from utils.rands import slugify_new
 
 
@@ -119,4 +120,15 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title, 4)
-        return super().save(*args, **kwargs)
+
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+
+        return super_save
