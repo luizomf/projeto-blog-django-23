bcbdf7de8809a56fb8bb9211c5394653f0e13c85
Luiz Otávio <luizomf@gmail.com>
2023-05-22 07:24:02 -0300

    Tags: usando ManyToManyField dentro do template do Django

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,4 @@
-from blog.views import category, created_by, index, page, post
+from blog.views import category, created_by, index, page, post, tag
 from django.urls import path
 
 app_name = 'blog'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -55,6 +55,23 @@ def category(request, slug):
     )
 
 
+def tag(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
 def page(request, slug):
     return render(
         request,
